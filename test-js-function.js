4870af14ec879a82a3a4cb2137e73b3b5db41349
rf-langleyunion <rainer@langleyunion.ca>
2024-11-22 13:59:56 -0800

    Update test-js-function.js

--- test-js-function.js
+++ test-js-function.js
@@ -26,9 +26,9 @@ function handleButtonClick() {
         resultDiv.style.color = 'blue';
     } else {
         // Reset styles if the address is not '190 Gladwin'
-        resultDiv.style.marginTop = '';
-        resultDiv.style.fontSize = '';
-        resultDiv.style.color = '';
+        resultDiv.style.marginTop = '20px';
+        resultDiv.style.fontSize = '16px';
+        resultDiv.style.color = 'green';
     }
     
     resultDiv.innerHTML = `The following residence has been registered with the CRA:<br>${address}<br>${city}, ${province} ${postal}`;
