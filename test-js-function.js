6229051030bcc229081eb3cca3d5f707687c8dc4
rf-langleyunion <rainer@langleyunion.ca>
2024-11-22 14:07:49 -0800

    Update test-js-function.js

--- test-js-function.js
+++ test-js-function.js
@@ -13,17 +13,20 @@ function handleButtonClick() {
 
     // Populate the result div with the input value, using <br> for new lines
     const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = `The following residence has been registered with the CRA:<br>${address}<br>${city}, ${province} ${postal}`;
 
     // Check if the address is '190 Gladwin' and apply styles if true
     if (address === '190 Gladwin') {
         resultDiv.style.marginTop = '20px';
         resultDiv.style.fontSize = '16px';
         resultDiv.style.color = 'blue';
+        resultDiv.innerHTML = `The following residence has been registered with the CRA:<br>${address}<br>${city}, ${province} ${postal}`;
+
     } else {
         // Reset styles if the address is not '190 Gladwin'
-        resultDiv.style.marginTop = '';
-        resultDiv.style.fontSize = '';
-        resultDiv.style.color = '';
+        resultDiv.style.marginTop = '20px';
+        resultDiv.style.fontSize = '35px';
+        resultDiv.style.color = 'red';
+        resultDiv.innerHTML = `OH MY GOD RUN`;
+
     }
 }
